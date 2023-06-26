e0527cb4c51b1694f76165536c0d4ee561e5903b
Alfonso Garcia <alfonso.garcia2324@gmail.com>
2023-06-26 09:26:45 -0700

    getting color uniforms from main.js

--- shaders/fragment-shader.glsl
+++ shaders/fragment-shader.glsl
@@ -1,6 +1,14 @@
 varying vec2 vUVs;
+uniform vec4 color1;
+uniform vec4 color2;
 
 void main() {
   // gl_FragColor = vec4(vUVs.x, vUVs.y, 0, 1.0);
-  gl_FragColor = vec4(vUVs, 0, 1.0);
+  // gl_FragColor = vec4(vUVs, 0, 1.0);
+
+  gl_FragColor = mix(
+    color1,
+    color2,
+    vUVs.x
+  );
 }
